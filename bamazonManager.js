49cfc12917d6e4c15f33b9922194de7bdabe14ae
NickSessa <nicholas.sessa@gmail.com>
2019-08-14 23:47:44 -0400

    fixed queries and code in the manager js

--- bamazonManager.js
+++ bamazonManager.js
@@ -11,53 +11,65 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "zaq1@WSXcde3",
+  password: "password",
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   mainMenu();
 });
 
-function mainMenu () {
+function mainMenu() {
   inquirer.prompt(
     {
       name: "choice",
       type: "list",
       message: "Select an option:",
-      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+      choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Quit"]
     })
-    .then (function(answer) {
+    .then(function (answer) {
       if (answer.choice === "View Products for Sale") {
         viewProducts();
       } else if (answer.choice === "View Low Inventory") {
         viewLowInventory();
       } else if (answer.choice === "Add to Inventory") {
         addInventory();
-      } else {
+      } else if (answer.choice === "Add New Product") {
         addNewProduct();
+      } else {
+        connection.end();
+        return
       }
-    }
-  });
+    });
 }
 
 function viewProducts() {
   var SQL = "SELECT * FROM products";
-  connection.query(SQL, function(err, res) {
-    console.log(res);
+  connection.query(SQL, function (err, res) {
+    if (err) throw err;
+    //console.log(res);
     console.table(res);
-    connection.end();
-  }
+    //connection.end();
+    mainMenu()
+  })
 }
 
 function viewLowInventory() {
-  var SQL = "SELECT * FROM products WHERE stock_quantity < 10";
-  connection.query(SQL, function(err, res) {
-    console.log(res);
-    console.table(res);
-    connection.end();
-  }
+  var SQL = 'SELECT * FROM products WHERE stock_quantity < 50';
+  connection.query(SQL, function (err, res) {
+    if (err) throw err;
+    //console.log(res);
+    //console.log("result length: " + res.length)
+    if (res.length == 0) {
+      console.log("\nNo items need to be restocked.\n")
+    } else {
+      console.table(res);
+    }
+
+    //connection.end();
+    mainMenu()
+  })
 }
 
 function addInventory() {
@@ -66,7 +78,7 @@ function addInventory() {
       name: "itemID",
       type: "input",
       message: "Enter the item id of the item: ",
-      validate: function(value) {
+      validate: function (value) {
         if (isNaN(value) === false) {
           return true;
         }
@@ -77,7 +89,7 @@ function addInventory() {
       name: "addQty",
       type: "input",
       message: "Enter the amount to add: ",
-      validate: function(value) {
+      validate: function (value) {
         if (isNaN(value) === false) {
           return true;
         }
@@ -85,27 +97,49 @@ function addInventory() {
       },
     }
   ])
-  .then(function(answer) {
+    .then(function (answer) {
 
-    //var newQty = answer.addQty + res.stock_quantity;
+      // first check to see if the item id exists
+      var item = answer.itemID;
 
-    var updateSQL = "UPDATE products SET stock_quantity=stock_quantity" + mysql.escape(answer.addQty) + " WHERE item_id=" + mySql.escape(answer.itemID);
-    connection.query(SQL, function(err, res) {
-      console.log(res);
-      console.table(res);
-      connection.end();
-    }
-  });
+      var SQL = 'SELECT * FROM products WHERE item_id=' + mysql.escape(item)
+
+      connection.query(SQL, function (err, res) {
+        if (err) throw err;
+        if (res.length == 0) {
+          console.log("Invalid ID.  Please select a valid ID.")
+          addInventory()
+        } else {
+          console.table(res)
+          var newQty = parseInt(answer.addQty) + parseInt(res[0].stock_quantity);
+          //console.log("old quantity: " + res[0].stock_quantity)
+          //console.log("new quantity: " + newQty)
+          var updateSQL = 'UPDATE products SET stock_quantity=' + mysql.escape(newQty) + ' WHERE item_id=' + mysql.escape(answer.itemID);
+          //console.log("SQL: " + updateSQL)
+          connection.query(updateSQL, function (err, res) {
+            if (err) throw err;
+            //console.log(res);
+            //console.table(res);
+            //connection.end();
+            connection.query(SQL, function (err2, res2) {
+              if (err2) throw err2;
+              console.table(res2)
+              mainMenu()
+            })
+
+          })
+
+        };
+      })
+    })
 }
-
-
 function addNewProduct() {
   inquirer.prompt([
     {
       name: "productName",
       type: "input",
       message: "Product Name: ",
-      
+
     },
     {
       name: "departmentName",
@@ -116,7 +150,7 @@ function addNewProduct() {
       name: "price",
       type: "input",
       message: "Price: ",
-      validate: function( value ) {
+      validate: function (value) {
         var valid = !isNaN(parseFloat(value));
         return valid || "Please enter a number";
       },
@@ -126,24 +160,33 @@ function addNewProduct() {
       name: "qty",
       type: "input",
       message: "Quantity: ",
-      validate: function( value ) {
+      validate: function (value) {
         var valid = !isNaN(parseFloat(value));
         return valid || "Please enter a number";
       },
       filter: Number
     }
   ])
-  .then(function(answer) {
-    var SQL = "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ?";
-    var values = [
-      answer.productName,
-      answer.departmentName,
-      answer.price,
-      answer.qty
-    ];
-    connection.query(SQL, [values], function(err, res) {
-      if (err) throw err;
-      console.log("Row added")
-      connection.end();
-  });
+    .then(function (answer) {
+
+      var SQL = connection.query(
+        'INSERT INTO products SET ?',
+        {
+          product_name: answer.productName,
+          department_name: answer.departmentName,
+          price: answer.price,
+          stock_quantity: answer.qty
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log("Row added")
+          //connection.end();
+          var SQL = 'SELECT * FROM products WHERE product_name=' + mysql.escape(answer.productName)
+          connection.query(SQL, function (err2, res2) {
+            if (err2) throw err2;
+            console.table(res2)
+            mainMenu()
+          })
+        });
+    })
 }
