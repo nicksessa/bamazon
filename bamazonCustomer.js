da9b33881614484bd9c34ed3939c46d917968c6e
NickSessa <nicholas.sessa@gmail.com>
2019-08-14 22:03:36 -0400

    fixed the customer.js file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,23 +11,24 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "zaq1@WSXcde3",
+  password: "password",
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   showInventory();
-  buyStuff();
+
 });
 
 function showInventory() {
   var SQL = "SELECT * FROM products";
-  connection.query(SQL, function(err, res) {
-    console.log(res);
+  connection.query(SQL, function (err, res) {
+    //console.log(res);
     console.table(res);
-    connection.end();
-  }
+    //connection.end();
+    buyStuff();
+  });
 }
 
 function buyStuff() {
@@ -36,8 +37,8 @@ function buyStuff() {
       name: "itemID",
       type: "input",
       message: "Select the ID of the item you wish to purchase. ",
-      validate: function ( value ) {
-        if (isNanvalue === false) {
+      validate: function (value) {
+        if (isNaN(value) === false) {
           return true;
         };
         return false || "You need to enter a number";
@@ -48,58 +49,49 @@ function buyStuff() {
       name: "quantity",
       type: "input",
       message: "How many? ",
-      validate: function( value ) {
-        var valid = !isNaN(parseFloat(value));
-        return valid || "Please enter a number";
-      },
-      filter: Number
+      validate: function (value) {
+        if (isNaN(value) === false) {
+          return true;
+        };
+        return false || "You need to enter a number";
+      }
     }
   ])
-  .then(function(answer) {
-    // Check inventory
-    var SQL = "SELECT item_id, product_name, stock_quantity FROM products WHERE item_id = " + mysql.escape(answer.itemID);
-    connection.query(SQL, function(err, res) {
-      if (err) throw err;
-      var totalAvailable = res.stock_quantity;
-      if (totalAvailable < answer.quantity) {
-        console.log("Insufficient quantity!");
-        return;
-      } else {
-         // subtract the quantity ordered from the totalAvailable
-         var newTotal = res.stock_quantity - answer.quantity;
-
-         //  then update the db
-         var updateSQL = "UPDATE products SET stock_quantity=" + mysql.escape(newTotal) + " WHERE item_id=" + mysql.escape(answer.itemID);
-         connection.query(updateSQL, function(err, res2) {
-           if (err) throw err;
-           console.log("Item purchased!")
-         });
-
-         // show the total price: quantity * price
-         var totalPrice = answer.quantity * res.price;
-         console.log("Total Price: $" + totalPrice)
-      }
-      connection.end();
-    })
-  });
+    .then(function (answer) {
+      // Check inventory
+      var SQL = 'SELECT item_id, product_name, stock_quantity, price FROM products WHERE item_id = ' + mysql.escape(answer.itemID);
+      //console.log("[SQL] " + SQL)
+      connection.query(SQL, function (err, res) {
+        if (err) throw err;
+        //console.log("res.stock_quantity: " + res[0].stock_quantity)
+        var totalAvailable = parseInt(res[0].stock_quantity);
+        //console.log("total avail: " + totalAvailable)
+        //console.log("num to purchase: " + answer.quantity)
+        if (totalAvailable < parseInt(answer.quantity)) {
+          console.log("Insufficient quantity!");
+          return;
+        } else {
+          // subtract the quantity ordered from the totalAvailable
+          var newTotal = parseInt(res[0].stock_quantity) - parseInt(answer.quantity);
+          //console.log("newTotal: " + newTotal)
+          // show the total price: quantity * price
+          var totalPrice = answer.quantity * parseFloat(res[0].price);
+          //console.log("res.price: " + parseFloat(res[0].price))
+          //console.log("Total Price: $" + totalPrice)
+          //  then update the db
+          var updateSQL = 'UPDATE products SET stock_quantity=' + mysql.escape(newTotal) + ' WHERE item_id=' + mysql.escape(answer.itemID);
+          //console.log("[updateSQL] " + updateSQL)
+          connection.query(updateSQL, function (err, res2) {
+            if (err) throw err;
+            if (answer.quantity > 1) {
+              console.log("Items purchased!")
+            } else {
+              console.log("Item purchased!")
+            }
+            console.log("Total Price: $" + totalPrice)
+          });
+        }
+        connection.end();
+      })
+    });
 }
-
-
-
-
-
-
-// example:
-{
-    type: "input",
-    name: "quantity",
-    message: "How many do you need",
-    validate: function( value ) {
-      var valid = !isNaN(parseFloat(value));
-      return valid || "Please enter a number";
-    },
-    filter: Number
-  },
-
-
-
